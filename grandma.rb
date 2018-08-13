75134bd2f320acc3706e3d355145ec30c32d83a7
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-13 02:46:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,15 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+def speak_to_grandma(spoken_by_grandma) 
+  
+  if (spoken_by_grandma=="I LOVE YOU GRANDMA!")
+      return   "I LOVE YOU TOO PUMPKIN"
+    elsif (spoken_by_grandma==spoken_by_grandma.upcase)
+        return "NO, NOT SINCE 1938!"
+      else
+        return "HUH? SPEAK UP, SONNY!"
+      end
+end
